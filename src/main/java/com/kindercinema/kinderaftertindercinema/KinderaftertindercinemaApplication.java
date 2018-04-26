5577e1c6e19ca33cc6901e6faa88fcce71fff339
Eitvidas Puras <peitvis@gmail.com>
2018-04-26 10:14:28 +0300

    Initial commit

---
+++ src/main/java/com/kindercinema/kinderaftertindercinema/KinderaftertindercinemaApplication.java
@@ -0,0 +1,12 @@
+package com.kindercinema.kinderaftertindercinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinderaftertindercinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinderaftertindercinemaApplication.class, args);
+    }
+}
