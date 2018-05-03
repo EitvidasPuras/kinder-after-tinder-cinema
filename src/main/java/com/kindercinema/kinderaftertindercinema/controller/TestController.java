2bb9fccd009d5ba88fb69b2c78180def9315f905
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2018-05-03 11:30:54 +0300

    Added example to test controller to get logged in user

--- src/main/java/com/kindercinema/kinderaftertindercinema/controller/TestController.java
+++ src/main/java/com/kindercinema/kinderaftertindercinema/controller/TestController.java
@@ -1,18 +1,29 @@
 package com.kindercinema.kinderaftertindercinema.controller;
 
+import com.kindercinema.kinderaftertindercinema.entity.User;
+import com.kindercinema.kinderaftertindercinema.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TestController {
 
-    @GetMapping("/test/{name}")
+    private final UserRepository userRepository;
+
+    @Autowired
+    public TestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/test")
     @PreAuthorize("hasAnyRole('CLIENT')")
-    public String test(@PathVariable String name, Model model) {
-        model.addAttribute("name", name);
+    public String test(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("name", user.getEmail());
         return "greetings";
     }
 }
