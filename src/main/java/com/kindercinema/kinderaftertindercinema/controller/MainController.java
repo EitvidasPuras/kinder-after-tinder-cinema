4303bffacaf9cf396da1a1501505b2b6418af488
Marius Dura <marius.dura@ktu.edu>
2018-05-08 15:14:47 +0300

    Approved by Mindaugas

--- src/main/java/com/kindercinema/kinderaftertindercinema/controller/MainController.java
+++ src/main/java/com/kindercinema/kinderaftertindercinema/controller/MainController.java
@@ -10,14 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
-
-    private final UserRepository userRepository;
-
-    @Autowired
-    public TestController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+public class MainController {
 
     @GetMapping("/")
     @PreAuthorize("hasAnyRole('CLIENT')")
@@ -25,5 +18,15 @@ public class TestController {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("name", user.getEmail());
         return "greetings";
+    }
+
+    @GetMapping("/home")
+    public String mainPage() {
+        return "mainPage";
+    }
+
+    @GetMapping("/movies")
+    public String moviesPage() {
+        return "moviesPage";
     }
 }
