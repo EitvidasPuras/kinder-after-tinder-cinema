c38544be0f2e94589f2f88422a2a4667bff5ff32
Marius Dura <marius.dura@ktu.edu>
2018-05-15 16:24:54 +0300

    Removed x from navbar, edited ticketPurchasePage, fixed cashier ticketPurchasePage design, now it's awesome. :)

--- src/main/java/com/kindercinema/kinderaftertindercinema/controller/client/TicketPurchaseController.java
+++ src/main/java/com/kindercinema/kinderaftertindercinema/controller/client/TicketPurchaseController.java
@@ -1,7 +1,8 @@
 package com.kindercinema.kinderaftertindercinema.controller.client;
 
-import com.kindercinema.kinderaftertindercinema.entity.Movie;
+import com.kindercinema.kinderaftertindercinema.entity.Seat;
 import com.kindercinema.kinderaftertindercinema.entity.Session;
+import com.kindercinema.kinderaftertindercinema.repository.RowRepository;
 import com.kindercinema.kinderaftertindercinema.repository.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,25 +10,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import java.util.List;
+
+@Controller("clientTicketPurchase")
 @RequestMapping("/session")
 public class TicketPurchaseController {
 
     private final SessionRepository sessionRepository;
+    private final RowRepository rowRepository;
 
     @Autowired
-    public TicketPurchaseController(SessionRepository sessionRepository) {
+    public TicketPurchaseController(SessionRepository sessionRepository, RowRepository rowRepository) {
         this.sessionRepository = sessionRepository;
+        this.rowRepository = rowRepository;
     }
 
-    @GetMapping("/{session_id}")
+    @GetMapping("/{sessionId}")
     public String openTicketPurchasePage(@PathVariable int sessionId, Model model) {
         Session session = sessionRepository.findById(sessionId).get();
-        Movie movie = session.getMovie();
-
-
+        model.addAttribute("sessionas", session);
+        model.addAttribute("sessionMovie", session.getMovie());
         return "client/ticketPurchasePage";
+    }
+
+    @GetMapping("/id/{sessionId}")
+    @ResponseBody
+    public Session ss(@PathVariable String sessionId, Model model) {
+        Session session = sessionRepository.findById(Integer.parseInt(sessionId)).get();
+
+        return session;
+    }
+
+    @GetMapping("/row/{rowId}")
+    @ResponseBody
+    public List<Seat> getSeatsByRow(@PathVariable int rowId) {
+        return rowRepository.findById(rowId).get().getSeats();
     }
 
 
---
+++ src/main/java/com/kindercinema/kinderaftertindercinema/repository/RowRepository.java
@@ -0,0 +1,7 @@
+package com.kindercinema.kinderaftertindercinema.repository;
+
+import com.kindercinema.kinderaftertindercinema.entity.Row;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RowRepository extends JpaRepository<Row, Integer> {
+}
