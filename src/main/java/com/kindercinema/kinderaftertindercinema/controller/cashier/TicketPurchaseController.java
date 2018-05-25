ae0a735ed75ebf82cf7acd0a2ceb1d6f942ab78a
Eitvidas Puras <peitvis@gmail.com>
2018-05-25 14:13:21 +0300

    Added functionality that if a cashier sells a ticket it saves it to the database

--- src/main/java/com/kindercinema/kinderaftertindercinema/controller/cashier/TicketPurchaseController.java
+++ src/main/java/com/kindercinema/kinderaftertindercinema/controller/cashier/TicketPurchaseController.java
@@ -1,23 +1,33 @@
 package com.kindercinema.kinderaftertindercinema.controller.cashier;
 
 import com.kindercinema.kinderaftertindercinema.entity.Hall;
+import com.kindercinema.kinderaftertindercinema.entity.Seat;
+import com.kindercinema.kinderaftertindercinema.entity.Session;
+import com.kindercinema.kinderaftertindercinema.entity.Ticket;
+import com.kindercinema.kinderaftertindercinema.repository.SeatRepository;
 import com.kindercinema.kinderaftertindercinema.repository.SessionRepository;
+import com.kindercinema.kinderaftertindercinema.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @Controller("cashierTicketPurchaseController")
 @RequestMapping("/cashier/ticketpurchase")
 public class TicketPurchaseController {
 
     private final SessionRepository sessionRepository;
+    private final TicketRepository ticketRepository;
+    private final SeatRepository seatRepository;
 
     @Autowired
-    public TicketPurchaseController(SessionRepository sessionRepository) {
+    public TicketPurchaseController(SessionRepository sessionRepository,
+                                    TicketRepository ticketRepository,
+                                    SeatRepository seatRepository) {
         this.sessionRepository = sessionRepository;
+        this.ticketRepository = ticketRepository;
+        this.seatRepository = seatRepository;
     }
 
     @GetMapping
@@ -29,5 +39,19 @@ public class TicketPurchaseController {
     @ResponseBody
     public Hall getHallBySessionId(@PathVariable int sessionId) {
         return sessionRepository.findById(sessionId).get().getHall();
+    }
+
+    @PostMapping("/save")
+    public String saveTicket(@RequestParam("sessionIdName") int sessionId, @RequestParam("seatSelect") int seatId) {
+        Seat seat = seatRepository.findById(seatId).get();
+        Session session = sessionRepository.findById(sessionId).get();
+        Ticket ticket = new Ticket();
+        ticket.setPayDate(new Date());
+        ticket.setCanceled(false);
+        ticket.setReserved(false);
+        ticket.setSession(session);
+        ticket.setSeat(seat);
+        ticketRepository.saveAndFlush(ticket);
+        return "redirect:/cashier/ticketpurchase";
     }
 }
